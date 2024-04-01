634feb09732a53106e12c025ae21c606ff1417c5
autumnesponda <autumnesponda@knights.ucf.edu>
2024-04-01 17:57:14 -0400

    import raw assets from previous site

---
+++ src/app/constants.ts
@@ -0,0 +1,169 @@
+export abstract class Constants {
+    static MEDIA_PREFIX = 'assets/media/';
+
+    static EVA_FILENAMES: string[][] =
+        [
+            [ 'eoe_clip_with_cels.mp4', 'EOE_00_01.jpeg', 'EOE_00_02.jpeg', 'EOE_00_03.jpeg', 'EOE_00_04.jpeg', 'EOE_00_05.jpeg',
+                'EOE_00_06.jpeg', 'EOE_00_07.jpeg', 'EOE_00_08.jpeg', 'EOE_00_09.jpeg', 'EOE_00_10.jpeg', 'EOE_00_11.jpeg', 'EOE_00_12.jpeg',
+                'EOE_00_13.jpeg', 'EOE_00_14.jpeg', 'EOE_00_15.jpeg', 'EOE_00_16.jpeg'
+            ],
+            [
+                'eoe_mpeva_with_cels.mp4', 'EOE_01_B1_CEL.jpeg', 'EOE_01_B1_DOUGA.jpeg', 'EOE_01_B2_CEL.jpeg', 'EOE_01_B2_DOUGA.jpeg'
+            ],
+            [
+                'EOE_02_A10.jpeg'
+            ],
+            [
+                'nge_14_with_cels.mp4', 'NGE_EPISODE13_FULL.png', 'NGE_01_00.png', 'NGE_01_01.png', 'NGE_01_02.png', 'NGE_01_03.png'
+            ],
+            [
+                'eoe_heliman_w_cels.mp4', 'EOE_03_combined.jpg', 'EOE_03_C17.jpg', 'EOE_03_D6.jpg'
+            ],
+            [
+                'wowmisto.jpg', 'NGE_misato_raw_00.jpg', 'NGE_misato_raw_01.jpg', 'NGE_misato_raw_02.jpg', 'NGE_misato_raw_03.jpg', 'NGE_misato_raw_04.jpg', 'NGE_misato_raw_05.jpg'
+            ],
+            [
+                'smallerflappybirds.mp4', 'EPSON017.jpg', 'EPSON015.jpg'
+            ]
+        ];
+
+    static JOJO_FILENAMES =
+        [
+            'jojo/CHARIOTCEL00108066.png|/jojo/CHARIOTMASTER00108150.png',
+
+            'jojo/fullcel.jpeg',
+            'jojo/layout01.jpeg',
+            'jojo/dougaB0.jpeg',
+
+            'jojo/correction.jpeg',
+            'jojo/dougaB1.jpeg',
+            'jojo/gengaB0.jpeg',
+
+            'jojo/gengaB1.jpeg',
+            'jojo/genga.jpeg',
+            'jojo/layoutA1.jpeg',
+
+            'jojo/dougaC6.jpg',
+            'jojo/dougaD2.jpg',
+            'jojo/layout02.jpg',
+
+            'jojo/layout03.jpg',
+            'jojo/correctionC2.jpg',
+            'jojo/correctionC3.jpg',
+        ]
+
+    static JOJO_CAPTIONS =
+        [
+            'A-10 cel of Silver Chariot from the first episode of the original 1993 six-episode OVA series (8th overall), entitled: "Iggy the Fool and N\'Doul the GEB (Pt. I)"',
+            'The full four-layer key pan cel (A1/B1/C6/D2). These are from the second episode of the original 1993 six-episode OVA series (9th overall), entitled: "Iggy the Fool and N\'Dool the GEB (Pt. II)."',
+            'The original layout for the entire pan shot. The note on top reads "3D shading to go with 94(C)" - the next shot in the sequence',
+            'Final douga for cel B-1 of the pan cel',
+            'Correction genga signed by award-winning animator Hiroyuki Kitakubo who was a chief director for this series. The note reads: "☆Animation Director~ Please fix the tear in the clothes here so it doesn\'t touch the red line because it interferes with the hat in cel D."',
+            'The original B-1 douga referenced in the previous image. Note how much longer the tear in the clothing is compared to the final cel.',
+            'The original B-1 genga',
+            'Updated B-1 genga reflecting the changes to the tear in the sleeve as requested by Kitakubo-san, as well as a more squinted expression by Mr. Joestar.',
+            'Genga for a shot of Mr. Jostar in the next cut',
+            'Final douga for cel A-1 of the pan cel',
+            'Final douga for cel C-6 of the pan cel',
+            'Final douga for cel D-2 of the pan cel',
+            '',
+            '',
+            '',
+            'Another correction genga signed by Kitakubo-san: "A little more tilt/slant for the hat is OK"',
+        ]
+
+    static GUNDAM_FILENAMES =
+        [
+            'other/newCenturyGundamX_%20A21E.png',
+            'other/newCenturyGundamX_%20A21E_douga.png',
+            'other/ggundamcel.png|/Other/ggundammaster.png',
+            'other/mobile_suit_victory_gundam_01.png',
+            'other/RMS-099%20Rick%20Dias.jpg',
+            'other/gundam0001cel.jpg',
+            'other/gundam0001douga.jpg',
+            'other/gundam000cel.jpg',
+            'other/gundam000douga.jpg'
+        ]
+
+    static GUNDAM_CAPTIONS =
+        [
+            'A-21 with color-copy background from After War Gundam X featuring the Air Master',
+            'Douga of the previous cel',
+            'B-19 of Devil Gundam from G Gundam Ep. 49 "God Gundam\'s Great Triumph: A Hopeful Future; Ready, Go!"',
+            'Cel A-22, cut 286 of LM111E03 Gunblaster from Victory Gundam Ep. 45 "Uso Dances with Dreams" ',
+            'Cel A-7 of Rick Dias from Zeta Gundam',
+            'Cel A-9, Cut 253 of RMS-019R Crouda Lancerow Custom from After War Gundam X',
+            'Douga of the previous cel',
+            'Cel A-4, Cut 96 of NRX-0013-CB Gundam Virsago Chest Break from After War Gundam X Ep. 36 "The Next War is the War We Wanted"',
+            'Douga of the previous cel'
+        ]
+
+    static GHOSTSTORIES_FILENAMES =
+        [
+            'other/ghost_stories_00.jpg|/Other/ghost_stories_00_master.png',
+            'other/ghost_stories_02.jpg|/Other/ghost_stories_02_master.png',
+            'other/ghost%20stories%2003.jpg',
+            'other/GHOST_STORIES_01_00.png',
+            'other/GHOST_STORIES_01_01.png'
+        ]
+
+    static GHOSTSTORIES_CAPTIONS =
+        [
+            'A-1 Douga From Episode 09: "Shirotabi: The Corpse That Wanders the Night." Note the lack of backpack and different shape of the face compared to the final frame from the TV show',
+            'Correction sheet for the previous douga; adding in the backpack, updating the ponytail, changing the eyelashes, and refining the face shape',
+            'Douga for the sweat drop, marking its starting and final locations',
+            '',
+            ''
+        ]
+
+    static FMA_FILENAMES =
+        [
+            'other/ed_fma.gif',
+            'other/fma00_01.jpg',
+            'other/fma00_00.jpg',
+            'other/fma00_02.jpg',
+            'other/dietlinde_douga.jpg|/Other/dietlinde_master.png',
+            'other/EPSON010.jpg',
+            'other/fma01_00.jpg',
+            'other/fma_douga_animated_00.gif'
+        ]
+
+
+    static FMA_CAPTIONS =
+        [
+            'A set of douga featuring the Fullmetal Alchemist himself, Edward Elrich, from the first episode of the series, "Those Who Challenge the Sun"',
+            '',
+            '',
+            '',
+            'A-1 douga featuring Dietlinde Eckhart from "Fullmetal Alchemist the Movie: Conqueror of Shamballa"',
+            'Correction genga from "Fullmetal Alchemist the Movie: Conqueror of Shamballa"',
+            '',
+            'A set of douga featuring Rudolf Hess from "Fullmetal Alchemist the Movie: Conqueror of Shamballa"'
+        ]
+
+    static OTHER_FILENAMES =
+        [
+            'other/pompoko.png',
+            'other/poopycat.jpg',
+            'other/Lassie_01.png',
+            'other/nardo02.png',
+            'other/nardo01.png',
+            'other/nardo00.png',
+            'other/KLK_ep10_front.jpg|/Other/KLK_ep10_master.png',
+            'other/Courage_storyboard_70.png',
+            'other/Courage_storyboard_87.png'
+        ]
+
+    static OTHER_CAPTIONS =
+        [
+            'Cels from the 1994 Studio Ghibli film Pom Poko. A-14,B-18,C-16, and D-11.',
+            '',
+            '',
+            '',
+            '',
+            '',
+            'A-1 Douga of Nonon Jakuzure from the final scene of Kill La Kill Ep. 10: "I Want to Know More about You"',
+            'Storyboard from "The Fog of Courage", a 2013 7-minute CGI pilot for a possible revival of Courage the Cowardly Dog. Signed by John R. Dilworth, the director and executive producer of the original series as well as this pilot',
+            'Storyboard from "The Fog of Courage", a 2013 7-minute CGI pilot for a possible revival of Courage the Cowardly Dog. Signed by John R. Dilworth, the director and executive producer of the original series as well as this pilot',
+        ]
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Constants } from '../../constants';
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
+
+  slides = [
+    {img: Constants.MEDIA_PREFIX + "eva/EOE_00_16.jpeg"},
+    {img: Constants.MEDIA_PREFIX + "jojo/cellthumbnail.jpg"},
+    {img: Constants.MEDIA_PREFIX + "eva/NGE_EPISODE13_FULL.png"},
+    {img: Constants.MEDIA_PREFIX + "eva/EOE_03_combined.jpg"},
+    {img: Constants.MEDIA_PREFIX + "other/RMS-099%20Rick%20Dias.jpg"},
+    {img: Constants.MEDIA_PREFIX + "eva/EOE_02_A10.jpeg"}
+
+  ];
 
 }
